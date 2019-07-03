61fc29b9a7e0f099371d1799e09f04aee0f8b5b5
maksim <duroff.83@yandex.ru>
2019-07-03 21:47:20 +0700

    v0.03

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # simple_drf
 (Python / Django Rest Framework)
+
+| Endpoint | HTTP Метод | CRUD Метод | Результат |  
+| :---- | :----: | :----: | :----: |
+| **/authors** | GET | READ | Список авторов |
+| **/books** | GET | READ | Список книг |
+| **/authors** | POST | CREATE | Новый автор |
+| **/books** | POST | CREATE | Новая книга |
+| **/books/:id** | GET | READ | Книга по id |
+| **/books/:id** | PUT | UPDATE | Изменение книги |
+| **/books/:id** | DELETE | DELETE | Удаление книги |
--- api/views.py
+++ api/views.py
@@ -13,7 +13,7 @@ class AuthorsView(APIView):
     serializer_class = AuthorsSerializator
 
     # list of authors + count
-    def get(self, request, format=None):
+    def get(self, request):
 
         all_authors = Author.objects.all()
         serializator = AuthorsSerializator(all_authors, many=True)
@@ -25,7 +25,7 @@ class AuthorsView(APIView):
         return Response(data)
 
     # create new author
-    def post(self, request, format=None):
+    def post(self, request):
         serializator = AuthorsSerializator(data=request.data)
         if serializator.is_valid():
             serializator.save()
@@ -38,7 +38,7 @@ class BooksView(APIView):
     serializer_class = BooksSerializator
 
     # list of books + count
-    def get(self, request, format=None):
+    def get(self, request):
         all_books = Book.objects.all()
         serializator = BooksSerializator(all_books, many=True)
 
@@ -49,7 +49,7 @@ class BooksView(APIView):
         return Response(data)
 
     # create new book
-    def post(self, request, format=None):
+    def post(self, request):
         serializator = BooksSerializator(data=request.data)
         if serializator.is_valid():
             serializator.save()
@@ -59,15 +59,37 @@ class BooksView(APIView):
 
 
 class BookDetailView(APIView):
+    serializer_class = BooksSerializator
+
+    def get_book(self, id):
+        try:
+            return Book.objects.get(pk=id)
+        except Book.DoesNotExist:
+            return HttpResponse(status=404)
+
 
     # get book info by id
     def get(self, request, book_id):
 
-        try:
-            book = Book.objects.get(pk=book_id)
-        except Book.DoesNotExist:
-            return HttpResponse(status=404)
-
+        book = self.get_book(book_id)
         serializator = BooksSerializator(book)
-
         return Response(serializator.data)
+
+
+
+    # update book by id
+    def put(self, request, book_id):
+
+        book = self.get_book(book_id)
+        serializator = BooksSerializator(book, data=request.data)
+        if serializator.is_valid():
+            serializator.save()
+            return Response(serializator.data)
+        return Response(serializator.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    # delete book by id
+    def delete(self, request, book_id):
+        book = self.get_book(book_id)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
